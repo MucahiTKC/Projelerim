78765c0c901bb4603f349a30c29986a448ffecfc
Mücahit Koç <mucahitkc13@gmail.com>
2020-05-25 18:48:38 +0300

    Create çift mi tek mi.py
    Girilen sayının çift mi tek mi olduğunu açıklar.

---
+++ çift mi tek mi.py
@@ -0,0 +1,12 @@
+print("\nBu program sayının tek mi çift mi olduğunu açıklar")
+while True:
+	sayı = input("\n\nSayıyı giriniz: ")
+	if int(sayı) % 2 == 0:
+		print("\nSayı çifttir")
+
+	else:
+		print("\nTektir")
+
+
+	
+
